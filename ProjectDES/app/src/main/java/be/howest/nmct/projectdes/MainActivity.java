91cfe6a98d37097bcfea7d84f5d652798b6c0b0f
KevinVanLancker <kevin.van.lancker@student.howest.be>
2015-04-16 18:23:19 +0200

    test github commit via android studio

--- ProjectDES/app/src/main/java/be/howest/nmct/projectdes/MainActivity.java
+++ ProjectDES/app/src/main/java/be/howest/nmct/projectdes/MainActivity.java
@@ -23,9 +23,6 @@ public class MainActivity extends Activity {
         }
     }
 
-    //test
-
-
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
