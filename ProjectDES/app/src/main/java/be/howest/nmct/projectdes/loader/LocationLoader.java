30b945dd07bfc8e4deff2c86322c4d9467bd580e
KevinVanLancker <kevin.van.lancker@student.howest.be>
2015-05-04 00:09:40 +0200

    de locaties worden nu gesorteerd en getoond

--- ProjectDES/app/src/main/java/be/howest/nmct/projectdes/loader/LocationLoader.java
+++ ProjectDES/app/src/main/java/be/howest/nmct/projectdes/loader/LocationLoader.java
@@ -14,6 +14,10 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
 
 import be.howest.nmct.projectdes.Location;
 
@@ -24,6 +28,8 @@ public class LocationLoader extends AsyncTaskLoader<Cursor> {
 
     private Cursor mCursor;
     private static final String url = "http://data.kortrijk.be/sport/outdoorlocaties.json";
+    private List<Location> locs;
+    //json in aparte file methode inlezen en gegevens in een List van locations steken, die list sorteren per sport en dan elke locatie in een row steken
 
     private final String[] mColumnNames = new String[]{
             BaseColumns._ID,
@@ -32,6 +38,71 @@ public class LocationLoader extends AsyncTaskLoader<Cursor> {
             Contract.SportLocatieColumns.COLUMN_LOCATIE_SPORT
     };
 
+    private List<Location> getLocations(){
+        InputStream input = null;
+        JsonReader reader = null;
+        List<Location> locations = new ArrayList<>();
+
+        try {
+            input = new URL(url).openStream();
+            reader = new JsonReader(new InputStreamReader(input, "UTF-8"));
+
+            reader.beginArray();
+            while(reader.hasNext()){
+                reader.beginObject();
+                Location loc = new Location();
+                String lat = "";
+                String lng = "";
+                while(reader.hasNext()) {
+                    String name = reader.nextName();
+                    if (name.equals("benaming")){
+                        loc.benaming = reader.nextString();
+                    }else if(name.equals("adres")){
+                        loc.adres = reader.nextString();
+                    }else if(name.equals("gemeente")){
+                        loc.gemeente = reader.nextString();
+                    }else if(name.equals("soort")){
+                        loc.soort = reader.nextString();
+                    }else if(name.equals("sport")){
+                        loc.sport = reader.nextString();
+                    }else if(name.equals("afmetingen")){
+                        if(reader.peek().equals(JsonToken.NULL)){
+                            reader.skipValue();
+                        }else{
+                            loc.afmeting = reader.nextString();
+                        }
+                    }else if(name.equals("y")){
+                        lat = reader.nextString();
+                    }else if(name.equals("x")){
+                        lng = reader.nextString();
+                    }
+                    else{
+                        reader.skipValue();
+                    }
+                }
+
+                loc.pos = new LatLng(Double.parseDouble(lat),Double.parseDouble(lng));
+                locations.add(loc);
+
+                reader.endObject();
+            }
+            reader.endArray();
+        }    catch(IOException ex){
+            ex.printStackTrace();
+        }finally {
+            try{
+                reader.close();
+            }catch(IOException ex){
+                ex.printStackTrace();
+            }
+            try {
+                input.close();
+            }catch(IOException ex){
+                ex.printStackTrace();
+            }
+        }
+        return locations;
+    }
     private static Object lock = new Object();
 
     public LocationLoader(Context context){
@@ -60,76 +131,27 @@ public class LocationLoader extends AsyncTaskLoader<Cursor> {
         synchronized (lock){
             if(mCursor != null) return;
 
+            locs = getLocations();
+            Collections.sort(locs, new Comparator<Location>() {
+                @Override
+                public int compare(Location lhs, Location rhs) {
+                    return lhs.sport.compareTo(rhs.sport);
+                }
+            });
+
+
             MatrixCursor cursor = new MatrixCursor(mColumnNames);
-            InputStream input = null;
-            JsonReader reader = null;
+            int id = 1;
 
-            try {
-                input = new URL(url).openStream();
-                reader = new JsonReader(new InputStreamReader(input, "UTF-8"));
-
-                int id = 1;
-                reader.beginArray();
-                while(reader.hasNext()){
-                    reader.beginObject();
-                    Location loc = new Location();
-                    String lat = "";
-                    String lng = "";
-                    while(reader.hasNext()) {
-                        String name = reader.nextName();
-                        if (name.equals("benaming")){
-                            loc.benaming = reader.nextString();
-                        }else if(name.equals("adres")){
-                            loc.adres = reader.nextString();
-                        }else if(name.equals("gemeente")){
-                            loc.gemeente = reader.nextString();
-                        }else if(name.equals("soort")){
-                            loc.soort = reader.nextString();
-                        }else if(name.equals("sport")){
-                            loc.sport = reader.nextString();
-                        }else if(name.equals("afmetingen")){
-                            if(reader.peek().equals(JsonToken.NULL)){
-                                reader.skipValue();
-                            }else{
-                                loc.afmeting = reader.nextString();
-                            }
-                        }else if(name.equals("y")){
-                            lat = reader.nextString();
-                        }else if(name.equals("x")){
-                            lng = reader.nextString();
-                        }
-                        else{
-                            reader.skipValue();
-                        }
-                    }
-
-                    loc.pos = new LatLng(Double.parseDouble(lat),Double.parseDouble(lng));
-
-                    MatrixCursor.RowBuilder row = cursor.newRow();
-                    row.add(id);
-                    row.add(loc.adres);
-                    row.add(loc.gemeente);
-                    row.add(loc.sport);
-                    id++;
-                    reader.endObject();
-                }
-                reader.endArray();
-                mCursor = cursor;
+            for (Location loc : locs){
+                MatrixCursor.RowBuilder row = cursor.newRow();
+                row.add(id);
+                row.add(loc.adres);
+                row.add(loc.gemeente);
+                row.add(loc.sport);
+                id++;
             }
-            catch(IOException ex){
-                ex.printStackTrace();
-            }finally {
-                try{
-                reader.close();
-                }catch(IOException ex){
-                    ex.printStackTrace();
-                }
-                try {
-                input.close();
-                }catch(IOException ex){
-                    ex.printStackTrace();
-                }
-            }
+            mCursor = cursor;
         }
     }
 
